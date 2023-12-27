eedbbc7f30719a924cbd06b1c8f2b76810849388
wilgar <wilenri341@hotmail.com>
2023-12-27 12:30:09 -0500

    Tipod de datos primtivos, numeros y  booleans.

---
+++ 07_numeros.js
@@ -0,0 +1,55 @@
+// Tipo entero y decimal
+
+let entero = 42;
+
+let decimal = 3.14;
+
+console.log(typeof entero, typeof decimal);
+
+// Notaciones cientificas
+
+let cientifico = 5e3;
+
+let infinito = Infinity;
+
+let noEsUnNumero = NaN;
+
+// Operaciones aritmeticas
+
+let suma = 3 + 4;
+
+let resta = 4 - 4;
+
+let mutiplicacion = 4 * 7;
+
+let division = 16/2;
+
+// Modulo y Exponenciacion
+
+let modulo = 15 % 8;
+
+let exponenciacion = 2 ** 3;
+
+// Precision
+
+let resultado = 0.1 + 0.2;
+
+console.log(resultado);
+
+console.log(resultado.toFixed(2));
+
+console.log(resultado === 0.3);
+
+// Operaciones avanzadas
+
+let raizCuadrada = Math.sqrt(16);
+
+let valorAbsoluto = Math.abs(-7);
+
+let aleatorio = Math.random();
+
+console.log(raizCuadrada);
+
+console.log(valorAbsoluto);
+
+console.log(aleatorio);
