8b857762bf440a3f7160b22f3bafee19831f1da3
wilgar <wilenri341@hotmail.com>
2024-01-04 13:44:11 -0500

    Condicional While, do While.

---
+++ 30_while.js
@@ -0,0 +1,27 @@
+/**
+ * while(condicion) {
+ *  codigo
+ * }
+ */
+
+let contador = 0;
+
+while(contador <= 100) { 
+    console.log(contador);
+    contador++;
+}
+
+
+
+/**
+ * do{
+ *  codigo
+ * } while(condicion);
+ */
+
+let contador2 = 0
+
+do{
+    console.log(contador2);
+    contador2++
+}while(contador2 <= 10);
